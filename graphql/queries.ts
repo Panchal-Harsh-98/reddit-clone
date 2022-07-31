a052f444d6936da4e56d8fc1b4b58d5dacacf46e
harsh panchal <harshpanchal.silverwing@gmail.com>
2022-07-31 15:37:56 -0400

    Feed and post component created Feed query created

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -9,3 +9,36 @@ export const GET_SUBREDDIT_BY_TOPIC = gql`
     }
   }
 `;
+
+export const GET_ALL_POST = gql`
+  query MyQuery {
+    getPostList {
+      image
+      body
+      id
+      username
+      title
+      subreddit_id
+      created_at
+      subreddit {
+        topic
+        id
+        created_at
+      }
+      votes {
+        created_at
+        id
+        post_id
+        upvote
+        username
+      }
+      comments {
+        id
+        post_id
+        text
+        username
+        created_at
+      }
+    }
+  }
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import Feed from '../components/Feed';
 import Header from '../components/Header';
 import PostBox from '../components/PostBox';
 
@@ -15,7 +16,9 @@ const Home: NextPage = () => {
       <PostBox />
 
       {/* feed */}
-      <div className='flex'></div>
+      <div className='flex'>
+        <Feed />
+      </div>
     </div>
   );
 };
